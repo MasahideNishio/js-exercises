45e978cb4986cd92cc62c2e69036006a70200b02
Nishio Masahide (西尾 公秀) <masahide.nishio@jp.ricoh.com>
2024-09-24 21:13:15 +0900

    12

---
+++ ch15.01-03/ex12/index.js
@@ -0,0 +1,8 @@
+// 勝手にjavascript:が消されてしまうのでコメントアウト
+// https://tenki.jp/　で郵便番号を自動で入れてその地域の天気を表示するブックマークレット
+// javascript:(function () {
+//   const inputField = document.getElementById("keyword");
+//   const searchButton = document.getElementById("btn");
+//   inputField.value = "143-0027";
+//   searchButton.click();
+// })();
