77abe993c8448c3c885acf73249463a72e162fad
Nishio Masahide (西尾 公秀) <masahide.nishio@jp.ricoh.com>
2024-10-20 21:45:09 +0900

    ex03-04,07

--- ch15.04-10/ex04/TOC.js
+++ ch15.04-10/ex04/TOC.js
@@ -1,5 +1,3 @@
-/* ブックマークレットとしてブラウザに登録する場合は以下のコードの先頭に javascript: を追加 */
-/* // から始まるコメントはブックマークレット内で正しく動作しないことがあるため注意 */
 (() => {
   /* NOTE: ブックマークレットによって実行する想定のため DOMContentLoaded のイベントで実行しない */
   let toc = document.querySelector("#TOC");
@@ -43,34 +41,47 @@
       `;
 
   /* NOTE: 以下は書籍のサンプルと同じ */
-  let headings = document.querySelectorAll("h2,h3,h4,h5,h6");
-  let sectionNumbers = [0, 0, 0, 0, 0];
-  for (let heading of headings) {
+  const headings = document.querySelectorAll("h2,h3,h4,h5,h6");
+  const sectionNumbers = [0, 0, 0, 0, 0];
+  for (const heading of headings) {
     if (heading.parentNode === toc) {
       continue;
     }
-    let level = parseInt(heading.tagName.charAt(1)) - 1;
+    const level = parseInt(heading.tagName.charAt(1)) - 1;
     sectionNumbers[level - 1]++;
     for (let i = level; i < sectionNumbers.length; i++) {
       sectionNumbers[i] = 0;
     }
-    let sectionNumber = sectionNumbers.slice(0, level).join(".");
-    let span = document.createElement("span");
+    const sectionNumber = sectionNumbers.slice(0, level).join(".");
+    const span = document.createElement("span");
     span.className = "TOCSectNum";
     span.textContent = sectionNumber;
     heading.prepend(span);
 
-    let anchor = document.createElement("a");
-    let fragmentName = `TOC${sectionNumber}`;
+    const anchor = document.createElement("a");
+    const fragmentName = `TOC${sectionNumber}`;
     anchor.name = fragmentName;
     heading.before(anchor);
     anchor.append(heading);
 
-    let link = document.createElement("a");
+    const link = document.createElement("a");
     link.href = `#${fragmentName}`;
     link.innerHTML = heading.innerHTML;
 
-    let entry = document.createElement("div");
+    /* スムーズにスクロールさせる */
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = document.querySelector(`a[name="${fragmentName}"]`);
+      if (!target) {
+        return;
+      }
+      target.scrollIntoView({
+        behavior: "smooth", // スムーズにスクロール
+        block: "start", // 画面の先頭にターゲットを表示
+      });
+    });
+
+    const entry = document.createElement("div");
     entry.classList.add("TOCEntry", `TOCLevel${level}`);
     entry.append(link);
     toc.append(entry);
