5eb01d9eda73a9a37eea69a62eb33a04d5d9864d
Nishio Masahide (西尾 公秀) <masahide.nishio@jp.ricoh.com>
2024-11-11 22:01:28 +0900

    ex01

--- ch15.11-15/ex01/contents/index.js
+++ ch15.11-15/ex01/contents/index.js
@@ -1,14 +1,25 @@
 const form = document.querySelector("#new-todo-form");
 const list = document.querySelector("#todo-list");
 const input = document.querySelector("#new-todo");
+console.log("cookie : ", document.cookie);
 
 document.addEventListener("DOMContentLoaded", async () => {
   // TODO: ここで API を呼び出してタスク一覧を取得し、
   // 成功したら取得したタスクを appendToDoItem で ToDo リストの要素として追加しなさい
+  try {
+    const response = await fetch("/api/tasks"); // タスク一覧を取得
+    if (!response.ok) throw new Error("Failed to GET tasks");
+
+    const data = await response.json();
+    data.items.forEach((task) => appendToDoItem(task)); // タスクを１つずつ追加
+  } catch (error) {
+    alert(error.message);
+  }
 });
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   // TODO: ここで form のイベントのキャンセルを実施しなさい (なぜでしょう？)
+  e.preventDefault();
 
   // 両端からホワイトスペースを取り除いた文字列を取得する
   const todo = input.value.trim();
@@ -21,6 +32,24 @@ form.addEventListener("submit", (e) => {
 
   // TODO: ここで API を呼び出して新しいタスクを作成し
   // 成功したら作成したタスクを appendToDoItem で ToDo リストの要素として追加しなさい
+
+  try {
+    // 新しいTaskをPOSTする
+    const response = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({ name: todo }),
+    });
+
+    if (!response.ok) throw new Error("Failed to POST task");
+
+    const newTask = await response.json();
+    appendToDoItem(newTask);
+  } catch (error) {
+    alert(error.message);
+  }
 });
 
 // API から取得したタスクオブジェクトを受け取って、ToDo リストの要素を追加する
@@ -35,11 +64,48 @@ function appendToDoItem(task) {
   const toggle = document.createElement("input");
   // TODO: toggle が変化 (change) した際に API を呼び出してタスクの状態を更新し
   // 成功したら label.style.textDecorationLine を変更しなさい
+  toggle.type = "checkbox";
+  toggle.checked = task.status === "completed";
 
+  toggle.addEventListener("change", async () => {
+    try {
+      const response = await fetch(`/api/tasks/${task.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify({
+          status: toggle.checked ? "completed" : "active",
+        }),
+      });
+
+      if (!response.ok) throw new Error("Failed to update task status");
+
+      label.style.textDecorationLine = toggle.checked ? "line-through" : "none";
+    } catch (error) {
+      alert(error.message);
+    }
+  });
   const destroy = document.createElement("button");
   // TODO: destroy がクリック (click) された場合に API を呼び出してタスク を削除し
   // 成功したら elem を削除しなさい
+  destroy.textContent = "❌";
+  destroy.addEventListener("click", async () => {
+    try {
+      const response = await fetch(`/api/tasks/${task.id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) throw new Error("Failed to delete task");
 
+      elem.remove();
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+
+  elem.appendChild(toggle);
+  elem.appendChild(label);
+  elem.appendChild(destroy);
   // TODO: elem 内に toggle, label, destroy を追加しなさい
   list.prepend(elem);
 }
